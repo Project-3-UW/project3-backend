214e54a0bd11af203a9bbb800979cb60ce8578db
eliglezz <eliglezz@users.noreply.github.com>
2021-12-01 01:15:42 -0800

    added imgkit routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize = require("./config/connection.js")
 var cors = require('cors')
+// const ImageKit = require('imagekit');
 
 
 
@@ -14,7 +15,12 @@ const routes = require("./controllers");
 app.use(cors())
 app.use(express.static("public"));
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+      "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
